6469b466d5a96ce3bb59276e0acf592a815a8fb5
yamilsaad <yamilsaad00@gmail.com>
2023-11-01 12:54:28 -0300

    cambios

--- app/api/api_excel.py
+++ app/api/api_excel.py
@@ -1,6 +1,11 @@
 import openpyxl
 import xlrd
+import datetime
+import locale
+import zipfile
+
 from xlutils.copy import copy
+
 
 
 def generar_credixsa(datos: list, ruta_completa):
@@ -17,9 +22,13 @@ def generar_credixsa(datos: list, ruta_completa):
     workbook.close()
 
 
-def generar_veraz(datos: list, ruta_completa):
-    archivo_lectura = xlrd.open_workbook(ruta_completa)
-    claves_a_incluir = ['APENOM', 'DNI']
+def generar_veraz(datos: list):
+
+    archivo_excel = "AFECTACIONES_VERAZ_C23057_202307_INI.xls"
+    archivo_lectura = xlrd.open_workbook(archivo_excel)
+
+    claves_a_incluir = ['Apellido', 'DNI']
+
     archivo_escritura = copy(archivo_lectura)
     hoja_escritura = archivo_escritura.get_sheet(0)
     ultima_fila = 2
@@ -28,7 +37,13 @@ def generar_veraz(datos: list, ruta_completa):
             valor = dato.get(clave, '')
             hoja_escritura.write(ultima_fila, columna, valor)
         ultima_fila += 1
-    archivo_escritura.save(ruta_completa)
+    archivo_escritura.save(f'{mes}-{anio}-{archivo_excel}')
+
+    # Crear un archivo ZIP y agregar el archivo de Excel
+    with zipfile.ZipFile(f'{mes}-{anio}-AFECTACIONES_VERAZ.zip', 'w') as archivo_zip:
+        archivo_zip.write(f'{mes}-{anio}-{archivo_excel}')
+
+
 
 
 def generar_codesa(datos: list, ruta_completa):
@@ -42,4 +57,19 @@ def generar_codesa(datos: list, ruta_completa):
             valor = dato.get(clave, '')
             hoja_escritura.write(ultima_fila, columna, valor)
         ultima_fila += 1
-    archivo_escritura.save(ruta_completa)
+    archivo_escritura.save(f'{mes}-{anio}-{archivo_excel}')
+
+
+def generar_catamarca(datos: list):
+    archivo_excel = "AFECTACIONES - CATAMARCA.xls"
+    archivo_lectura = xlrd.open_workbook(archivo_excel)
+    claves_a_incluir = ['Documento', 'Apellido_y_Nombre', 'Fecha_Acuerdo', 'Codigo', 'Monto', 'Cuotas', 'Importe_Cuotas', '1_Vencimiento']
+    archivo_escritura = copy(archivo_lectura)
+    hoja_escritura = archivo_escritura.get_sheet(0)
+    ultima_fila = 4
+    for dato in datos:
+        for columna, clave in enumerate(claves_a_incluir):
+            valor = dato.get(clave, '')
+            hoja_escritura.write(ultima_fila, columna, valor)
+        ultima_fila += 1
+    archivo_escritura.save(f'{mes}-{anio}-{archivo_excel}')
